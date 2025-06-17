5a8cb3ec36db9767391737c63b4b486a07e81bec
Alex Clivio <amrclivio@gmail.com>
2025-06-16 21:47:37 -0700

    add files, add basic calc functionality, add html shell

---
+++ index.js
@@ -0,0 +1,34 @@
+const add = function(num1, num2) {
+  return num1 + num2
+}
+
+const subtract = function(num1, num2) {
+  return num1 - num2
+}
+
+const divide = function(num1, num2) {
+  return num1 / num2
+}
+
+const multiply = function(num1 , num2) {
+  return num1 * num2
+}
+
+let number1;
+let number2;
+let operator;
+
+const operate = function(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
+
+console.log(operate('+', 10, 2));
